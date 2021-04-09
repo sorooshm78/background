3371da5de32d68ae824a9bf5afacf599266dab1e
sorooshm78@gmail.com <sorooshm78@gmail.com>
2021-04-09 19:50:55 +0430

    change gitignore

--- start.py
+++ start.py
@@ -3,11 +3,10 @@ import set
 
 image_name = 'bing'
 link = 'https://www.bing.com/'
-image_file = './image/%s.jpg' % image_name
-path_image_file = '~/Desktop/work/background/image/%s.jpg' % image_name
+path_image_file = '/home/soroush/Desktop/work/background/image/%s.jpg' % image_name
 
 print("downloading ...")
-download.download_image(link, image_file)
+download.download_image(link, path_image_file)
 print("download %s.jpg" % image_name)
 
 print("set background %s.jpg" % image_name)
