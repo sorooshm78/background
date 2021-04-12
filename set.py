7665c8e914f490359d781d3c06888da66b98e7ea
sorooshm78@gmail.com <sorooshm78@gmail.com>
2021-04-12 21:01:21 +0430

    add exception handling

--- set.py
+++
@@ -1,5 +0,0 @@
-import os
-
-def set_image(file_path):
-	command = "/usr/bin/gsettings set org.gnome.desktop.background picture-uri %s" %file_path
-	os.system(command)
--- start.py
+++ start.py
@@ -1,13 +1,38 @@
-import download
-import set
+import os
+import re
+import requests
+from bs4 import BeautifulSoup
 
-image_name = 'bing'
+image_name = 'background'
 link = 'https://www.bing.com/'
-path_image_file = '/home/soroush/Desktop/work/background/image/%s.jpg' % image_name
+path_image_file = '/home/soroush/Desktop/work/background/download/%s.jpg' % image_name
 
-print("downloading ...")
-download.download_image(link, path_image_file)
-print("download %s.jpg" % image_name)
+# Download
+print("downloading ...") 
+try:
+	page = requests.get(link)
+	soup = BeautifulSoup(page.content, 'html.parser')
+	source = soup.find('div', class_='img_cont')
 
-print("set background %s.jpg" % image_name)
-set.set_image(path_image_file)
+	image_url = link
+	image_url += re.findall('.*url\((.*)\)',str(source))[0]
+
+	img_data = requests.get(image_url).content
+
+	with open(path_image_file, 'wb') as handler:
+		handler.write(img_data)
+		command = "cp ./download/%s.jpg ./local" %image_name
+		os.system(command)
+	
+	print("download %s.jpg" %image_name)
+	
+	# Set background
+	command = "/usr/bin/gsettings set org.gnome.desktop.background picture-uri %s" %path_image_file
+	os.system(command)
+	print("set background")
+
+except:
+	print("Internet not connect")
+	command = "/usr/bin/gsettings set org.gnome.desktop.background picture-uri /home/soroush/Desktop/work/background/local/%s.jpg" %image_name
+	os.system(command)
+	print("set local background")
