e0690f06abac5b92449a60568fd5a7be8ef7313b
sorooshm78@gmail.com <sorooshm78@gmail.com>
2021-04-20 19:38:32 +0430

    save all background and set random from downloaded file

--- start.py
+++ start.py
@@ -1,15 +1,29 @@
+import random
 import os
 import re
 import requests
 from bs4 import BeautifulSoup
 
-image_name = 'background'
-link = 'https://www.bing.com/'
-path_image_file = '/home/soroush/Desktop/work/background/download/%s.jpg' % image_name
+def write_file(filename, number):
+	with open(filename, 'w') as file:
+		file.write(str(number))
+
+def read_file(filename):
+	with open(filename, 'r') as file:
+		return file.read().strip()
 
 # Download
 print("downloading ...") 
 try:
+	image_name = read_file('index.txt')
+
+	number = int(image_name)
+	number += 1
+	write_file('index.txt', number)
+
+	link = 'https://www.bing.com/'
+	path_image_file = '/home/soroush/Desktop/work/background/download/%s.jpg' % image_name
+
 	page = requests.get(link)
 	soup = BeautifulSoup(page.content, 'html.parser')
 	source = soup.find('div', class_='img_cont')
@@ -21,18 +35,17 @@ try:
 
 	with open(path_image_file, 'wb') as handler:
 		handler.write(img_data)
-		command = "cp ./download/%s.jpg ./local" %image_name
-		os.system(command)
-	
+
 	print("download %s.jpg" %image_name)
-	
+
 	# Set background
 	command = "/usr/bin/gsettings set org.gnome.desktop.background picture-uri %s" %path_image_file
 	os.system(command)
 	print("set background")
 
 except:
+	image_name = random.randint(1, int(read_file('index.txt')))
 	print("Internet not connect")
-	command = "/usr/bin/gsettings set org.gnome.desktop.background picture-uri /home/soroush/Desktop/work/background/local/%s.jpg" %image_name
+	command = "/usr/bin/gsettings set org.gnome.desktop.background picture-uri /home/soroush/Desktop/work/background/download/%s.jpg" %image_name
 	os.system(command)
 	print("set local background")
